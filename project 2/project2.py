e983e2da7f0667b2fa0e009f86bc8ccb821f8d91
jdavis211512 <Jwdavis2@bsu.edu>
2020-10-30 17:01:25 -0400

    initial

--- project 2/project2.py
+++ project 2/project2.py
@@ -1,44 +1,65 @@
 import pandas as pd
 import matplotlib.pyplot as plot
+from datetime import datetime
+
+
+#C:\Users\jdavi\PycharmProjects\CS421_project2\project 2
 def readCSV(filepath):
     choice = input("what company are you wanting to view?\n")
     choice.lower()
     if (choice == "amazon"):
-        return pd.read_csv(filepath)
+        dateCols = ['Date']
+        return pd.read_csv(filepath,parse_dates=dateCols)
     if(choice == "apple"):
-        return pd.read_csv(filepath)
-    if (choice == "google"):
-        return pd.read_csv(filepath)
+        dateCols = ['Date']
+        return pd.read_csv(filepath,parse_dates=dateCols)
+    if (choice == "google",):
+        dateCols = ['Date']
+        return pd.read_csv(filepath,parse_dates=dateCols)
     else:
         print("please choose one of the 3 files for the assignment.")
-        return readCSV()
+        return readCSV(filepath)
 
 def chooseFrame(dataframe):
 
     year = input("what is the year you want to see?\n")
     if (year=="2015"):
-        return dataframe.iloc[0:47]
+        return pd.date_range('2015-10-23','2015-12-31')
     if (year=="2016"):
-        return dataframe.iloc[48:299]
+        return pd.date_range('2016-1-1','2016-12-31')
     if (year=="2017"):
-        return dataframe.iloc[300:550]
+        return pd.date_range('2017-1-1','2017-12-31')
     if (year=="2018"):
-        return dataframe.iloc[551:801]
+        return pd.date_range('2018-1-1', '2018-12-31')
     if (year=="2019"):
-        return dataframe.iloc[802:1053]
+        return pd.date_range('2019-1-1','2019-12-31')
     if (year=="2020"):
-        return dataframe.iloc[1054:1258]
+        return pd.date_range('2020-1-1', '2020-10-22')
     else:
         print("please select a year")
         chooseFrame(dataframe)
 
-
+def plotthis(minimum,maximum,closing,date):
+    plot.plot(date,minimum,color='blue')
+    plot.plot(date,maximum, color='red')
+    plot.plot(date,closing,color='black')
+    plot.show()
+    # plot.plot(date,)
+def getmeans():
 
 
 def main():
     filepath = input("what is the filepath of the file you want to use?\n")
     data = readCSV(filepath)
-    yearFrame = chooseFrame(data)
+    yearRange = chooseFrame(data)
+    fixedFrame = data[data['Date'].isin(yearRange)]
+    minimumSeries = fixedFrame['Low']
+    maximumSeries = fixedFrame['High']
+    dailySeries = fixedFrame['Close']
+    dateSeries = fixedFrame['Date']
+    meanSeries = getmeans()
+    plotthis(minimumSeries,maximumSeries,dailySeries,dateSeries)
+
 
 
 
